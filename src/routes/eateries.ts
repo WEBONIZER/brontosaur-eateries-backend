4be2895df849c8b64feca2de18658ed4c279d7dc
Denis Ivanov <site.ivanov2014@yandex.ru>
2024-07-15 16:36:14 +0300

    Пытаюсь настроить 404

--- src/routes/eateries.ts
+++ src/routes/eateries.ts
@@ -9,7 +9,5 @@ eateries.delete('/:eateriesRoute', removeEaterie)
 eateries.get('/:eateriesRoute', getEateriesByName);
 eateries.patch('/:eateriesRoute/like', addLikeToEaterie);
 eateries.delete('/:eateriesRoute/like', removeLikeFromEaterie);
-//eateries.patch('/:blogRoute/unlike', removeLikeFromBlog);
-//eateries.patch('/:blogRoute/unviewing', removeViewingFromBlog);
 
 export default eateries; 
